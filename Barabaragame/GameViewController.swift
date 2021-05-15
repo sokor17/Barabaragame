b5ebfd1474acc2f04b0c21533b8889b4ed788d02
YuKono <cosmos17128@gmail.com>
2021-05-16 00:23:28 +0900

    最終版

--- Barabaragame/GameViewController.swift
+++ Barabaragame/GameViewController.swift
@@ -21,7 +21,7 @@ class GameViewController: UIViewController {
     
     let width:CGFloat = UIScreen.main.bounds.size.width //画面の幅　iphoneの画面サイズを取得する
     var positionX:[CGFloat] = [0.0,0.0,0.0]//画像の位置の配列 (同じものを使う時のまとめ)
-    var dx:[CGFloat]=[1.0,0.5,-1.0]//動かす幅の配列　横に動かすのでx
+    var dx:[CGFloat]=[2.0,0.5,-2.0]//動かす幅の配列　横に動かすのでx
     
     func start(){
         //結果ラベルを隠す
@@ -53,17 +53,56 @@ class GameViewController: UIViewController {
         if timer.isValid == true{
             timer.invalidate()
         }
+        //スコアを表示する
+        for i in 0..<3 {
+            //スコアの計算をする
+            score = score - abs (Int(width/2 - positionX[i])) * 2
+        }
+        //結果ラベルにスコアを表示する
+        resultLabel.text = "Score : " + String(score)
+        //結果ラベルを出す
+        resultLabel.isHidden = false
+        
+        let highScore1: Int = defaults.integer(forKey: "score1") //userDefaultにscore1というキーを与える
+        let highScore2: Int = defaults.integer(forKey: "score2")
+        let highScore3: Int = defaults.integer(forKey: "score3")
+        
+        //ランキング1位の記録を更新したら
+        if score > highScore1 {
+            defaults.set(score,forKey: "score1")//あたらしいスコアを保存する
+            defaults.set(highScore1,forKey: "score2")//前回1位を2位に保存
+            defaults.set(highScore2,forKey: "score3")//前回2位を3位に保存
+        }else if score > highScore2{
+            defaults.set(score,forKey: "score2")//新しいスコアを保存する
+            defaults.set(highScore2,forKey: "score3")//前回2位を3位に保存
+        }else if score > highScore3{
+            defaults.set(score,forKey: "score3")
     }
+    }
+
+    
+    //リトライメソッドを作る
+    @IBAction func retry(){
+        //スコアの値をリセットする
+        score = 1000
+        //真ん中に戻す
+        positionX = [width/2,width/2,width/2]
+        if timer.isValid == false{
+            //スタートメソッドを呼び出す
+            self.start()
+        }
+    }
+    
+    //topに戻るメソッドを作る
+    @IBAction func toTop(){
+            self.dismiss(animated: true, completion: nil)
+        }
+  
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         positionX = [width/2,width/2,width/2]//画像の位置を画面幅の中心にする
         self.start() //スタートメソッドを呼び出す
-    }
-    
- 
-    
-
-   
-
+         }
 }
--- Barabaragame/RankingViewController.swift
+++ Barabaragame/RankingViewController.swift
@@ -8,26 +8,29 @@
 import UIKit
 
 class RankingViewController: UIViewController {
-
+    
+    //スコアの表示
+    @IBOutlet var rankingLabel1: UILabel!
+    @IBOutlet var rankingLabel2: UILabel!
+    @IBOutlet var rankingLabel3: UILabel!
+    //トップに戻るボタン
+    @IBAction func toTop(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    //スコアの保存の変数
+    let defaults: UserDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //score1とランキング1位を結びつける
+            rankingLabel1.text = String(defaults.integer(forKey: "score1"))
+            rankingLabel2.text = String(defaults.integer(forKey: "score2"))
+            rankingLabel3.text = String(defaults.integer(forKey: "score3"))
         
         
-
-        // Do any additional setup after loading the view.
-    }
+  }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
 
 
